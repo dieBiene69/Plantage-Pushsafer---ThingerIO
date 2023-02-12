a7d167ee44d36cb3632babaf1e41231ef967cbe6
Marie <marie.pannier@smail.th-koeln.de>
2023-02-12 21:54:09 +0100

    secrets

---
+++ include/secrets.h
@@ -0,0 +1,9 @@
+#define USERNAME ""
+#define DEVICE_ID ""
+#define DEVICE_CREDENTIAL ""
+#define SSID ""
+#define SSID_PASSWORD ""
+#define PushsaferKey "" // Pushsafer private or alias key
+
+char ssid[] = "";     // your network SSID (name)
+char password[] = ""; // your network key
